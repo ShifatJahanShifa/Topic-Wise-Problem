5b1b9a3217074c307566f7ab1cf8c33dfcc26dfa
Shifa <121536886+ShifatJahanShifa@users.noreply.github.com>
2023-12-24 12:38:50 +0600

    enjoyed

---
+++ Binary Search/Building an Aquarium.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef int long long llt;
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int n;cin>>n;
+        llt x;cin>>x;
+        vector<llt>a(n);
+        for(auto &it:a) cin>>it;
+        llt l=1,r=2e9+10;
+        while (l+1LL<r)
+        {
+            llt mid=l+(r-l)/2;
+            llt total=0;
+            for (int i = 0; i <n; i++)
+            {
+                total+=max(0LL,mid-a[i]);
+            }
+            if(total>x) r=mid;
+            else l=mid;
+        }
+        
+        cout<<l<<"\n";
+    }
+}
